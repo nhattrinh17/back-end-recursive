cae0d5999e244b7dc36f69cd3189db49df39ba33
Trịnh Nhật <trinhminhnhatxt123@gmail.com>
2022-09-22 17:24:42 +0700

    Heroku

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors({ 
+    credentials: true, 
+    origin: true 
+}))
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+
+app.get('/', (req, res) => res.send("Server"))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
