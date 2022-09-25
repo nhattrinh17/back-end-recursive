acda8d4676fe213abcfeecc91c70fe9cf60635f5
Trịnh Nhật <trinhminhnhatxt123@gmail.com>
2022-09-25 08:19:04 +0700

    Commit modules userToken

--- src/models/userToken.js
+++ src/models/userToken.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const UserTokenSchema = {
+const UserTokenSchema = new Schema({
     idUser: { type: String, require: true },
-};
+    tokenUser: { type: String, require: true },
+});
+
+module.exports = mongoose.model('userToken', UserTokenSchema);
