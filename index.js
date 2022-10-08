6bddd8755854cd7e1b137d0286133fddb9194769
Trịnh Nhật <trinhminhnhatxt123@gmail.com>
2022-10-08 19:07:07 +0700

    fix

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ const router = require('./src/route');
 
 const app = express();
 
-// use cors
-app.use(cors());
-
-app.use('/uploads', express.static('uploads'));
+const corsOptions = {
+    origin: '*', // allow cors origin for all client
+};
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// use cors
+app.use(cors(corsOptions));
 
 dataBase.conect();
 
