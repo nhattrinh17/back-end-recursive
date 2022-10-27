d63df6fa23883ffff35948dc154b654cafb2c6f2
Trịnh Nhật <trinhminhnhatxt123@gmail.com>
2022-10-27 08:08:00 +0700

    fix 27/10

--- src/route/exam.js
+++ src/route/exam.js
@@ -6,7 +6,7 @@ const upload = require('../util/storageMulter');
 const router = express.Router();
 router.get('/', examController.getExams);
 router.get('/search', examController.getExamsBySearch);
-router.post('/add', authMiddleware.verifilyToken, upload.single('fileExam'), examController.addExam);
+router.post('/add', authMiddleware.verifilyToken, examController.addExam);
 router.put('/update/:id', authMiddleware.verifilyTokenAndAdminOrAuth, examController.updateExam);
 router.delete('/delete/:id', authMiddleware.verifilyTokenAndAdminOrAuth, examController.deleteExam);
 router.put(
